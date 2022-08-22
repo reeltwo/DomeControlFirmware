c1e815089ca3631e83c9b8802dd2dfea0e925488
rimim <nomail@thanks.com>
2022-08-22 10:02:07 -0400

    Disabling software serial before eeprom update is no longer necessary

--- DomeControlFirmware.ino
+++ DomeControlFirmware.ino
@@ -1570,29 +1570,7 @@ bool processDomePositionCommand(const char* cmd)
 static void updateSettings()
 {
     restoreDomeSettings();
-#ifdef COMMAND_SOFT_SERIAL
-    // We must disable software serial on the ESP while updating flash memory
-    // the software serial RX interrupt will otherwise try to access cached memory
-    // while cache is disabled.
-    COMMAND_SERIAL.end();
-    delay(200);
-#endif
-#ifdef DOME_DRIVE_SOFT_SERIAL
-    // We must disable software serial on the ESP while updating flash memory
-    // the software serial RX interrupt will otherwise try to access cached memory
-    // while cache is disabled.
-    DOME_DRIVE_SERIAL.end();
-    delay(200);
-#endif
     sSettings.write();
-#ifdef DOME_DRIVE_SOFT_SERIAL
-    // Reenable software serial.
-    configureDomeDrive();
-#endif
-#ifdef COMMAND_SOFT_SERIAL
-    // Reenable software serial.
-    configureDomeDrive();
-#endif
     Serial.println(F("Updated"));
 #ifdef USE_WIFI_WEB
     sUpdateSettings = false;
